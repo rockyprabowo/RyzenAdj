80282c81fd700c60e39c98115a642c81e2fa1756
Rocky Prabowo <rocky.prabowo@gmail.com>
2019-07-30 21:54:18 +0700

    Modify the delay for demo scripts

--- prebuilt/demo.sh
+++ prebuilt/demo.sh
@@ -3,4 +3,4 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-./ryzenadj --stapm-time=3600 --stapm-limit=27000 --fast-limit=30000 --slow-limit=27000 --tctl-temp=90 --vrmmax-current=45000 --reapply-every=500
+./ryzenadj --stapm-time=3600 --stapm-limit=27000 --fast-limit=30000 --slow-limit=27000 --tctl-temp=90 --vrmmax-current=45000 --reapply-every=3000
